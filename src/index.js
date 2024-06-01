976f3c56dc32c71ba42d1046af869b47652fd735
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-06-01 08:00:24 +0100

    Added function refresh weather and search city

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#current-temperature-value");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#current-city");
+
+    cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city){
+    let apiKey = "f0edft08a334d1a9c4eb5o0155c624af";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
